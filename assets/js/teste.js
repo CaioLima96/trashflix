b97da239af4c6de1030089bcae62c8b8966a224b
Hyan Araujo <hyan.araujoalmeida@gmail.com>
2021-09-23 07:17:42 -0300

    estilo do carousel

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -80,9 +80,9 @@ const arrMovie = [
     {ptbr: 'À Meia-Noite Levarei Sua Alma', ingles:'At midnight I will take your soul '},
     {ptbr: 'Caderno da Morte', ingles:'death Note'},
     {ptbr: "Velocipastor", ingles: "Velocipastor"},
-    {ptbr: "Os maus mortos ", ingles : "the evil dead"}
-    {ptbr: " ", ingles : ""}
-    {ptbr: " ", ingles : ""}
+    {ptbr: "Os maus mortos ", ingles : "the evil dead"},
+    {ptbr: " ", ingles : ""},
+    {ptbr: " ", ingles : ""},
 ]
 
 
