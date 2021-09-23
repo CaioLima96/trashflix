0fd57c087e4c1a8dff73fe50d35e2ba7844db5ce
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-09-23 17:05:33 -0300

    mudado a api do omdb no controller de s pra t

--- assets/src/controller/controller.js
+++ assets/src/controller/controller.js
@@ -1,3 +1,3 @@
 function nossosFilmes (nameIngles) {
-    return $.ajax({url: `https://www.omdbapi.com/?S=${nameIngles}&apikey=c377b618`, async: false}).responseJSON
+    return $.ajax({url: `https://www.omdbapi.com/?t=${nameIngles}&apikey=c377b618`, async: false}).responseJSON
 }
